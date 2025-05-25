01f5eeee803d6fd8ad46030a03234774fbabbac8
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-05-25 08:35:57 +0530

    try render deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // This replaces `next export` command
-  images: {
-    unoptimized: true, // needed for static export if you use next/image
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
